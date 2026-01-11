f2c001539d962d9f2ab1fdf910d8290d84a1513e
Tarun Goel <tarungoel1823@gmail.com>
2026-01-11 16:32:36 +0530

    navbar fix

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,22 @@
-export default function Navbar() {
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+export default function Navbar({ name = "Tarun" }) {
+  const firstLetter = name.charAt(0).toUpperCase();
+
   return (
-    <div className="h-14 bg-white shadow fixed left-64 right-0 flex items-center px-4">
-      Book Management
+    <div className="fixed top-0 left-58 right-4 rounded-lg mt-4 h-14 bg-white shadow flex items-center justify-between px-6 z-10">
+      
+      <h1 className="text-[#374151] text-xl  font-medium">
+        Welcome!
+      </h1>
+
+      <div className="flex items-center gap-2 cursor-pointer">
+        <div className="h-8 w-8 flex items-center justify-center rounded-full bg-[#4F46E5] text-white font-medium">
+          {firstLetter}
+        </div>
+        <KeyboardArrowDownIcon className="text-gray-600" />
+      </div>
+
     </div>
   );
 }
